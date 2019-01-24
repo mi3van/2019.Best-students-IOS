ef115b1b5a913a0384d44f240fc637c6a4bb259f
Ivan Kuzmin <K.Ivan@russianit.ru>
2019-01-24 11:26:59 +0400

    Create project

---
+++ My"Best"students/sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My"Best"students
+//
+//  Created by Ivan Kuzmin on 23/01/2019.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ My"Best"students/sources/models/ModelStudent.swift
@@ -0,0 +1,9 @@
+//
+//  ModelStudent.swift
+//  My"Best"students
+//
+//  Created by Ivan Kuzmin on 23/01/2019.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import Foundation
