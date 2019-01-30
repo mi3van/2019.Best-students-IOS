579d622e8204516b60aff080540abbfe8a1afb9a
Ivan Kuzmin <K.Ivan@russianit.ru>
2019-01-30 14:17:40 +0400

    Added NavigationController

---
+++ My"Best"students/sources/controllers/Main/MainNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  MainNavigationController.swift
+//  "Best" students
+//
+//  Created by Ivan Kuzmin on 30/01/2019.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
